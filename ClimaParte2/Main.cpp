ec134ca47d57775e1b15959adbf4f927b848fb4d
Hollowriter <deadgamergx2@hotmail.com.ar>
2017-05-25 20:00:50 -0300

    testeando parsear con XML

--- ClimaParte2/Main.cpp
+++ ClimaParte2/Main.cpp
@@ -4,13 +4,28 @@
 #include<new>
 #include<exception>
 #include<fstream>
+#include<vector>
 #include"rapidxml.hpp"
 #include"rapidxml_print.hpp"
 using namespace std;
 using namespace rapidxml;
 void main() {
-	/*xml_document<> thyDoc;
+	cout << "Comencing parsing document test..." << endl;
+	xml_document<> thyDoc;
 	xml_node<> * normal_node;
 	ifstream thyText("Prueba.xml");
-	thyDoc.parse<0>(thyText);*/
+	vector<char> buffer((istreambuf_iterator<char>(thyText)), istreambuf_iterator<char>());
+	buffer.push_back('\0');
+	thyDoc.parse<0>(&buffer[0]);
+	normal_node = thyDoc.first_node("query");
+	for (xml_node<> * channel_node = normal_node->first_node("query"); channel_node; channel_node = channel_node->next_sibling()) 
+		// Probando con el debugger, aca me tira un error en tiempo de ejecucion
+	{
+		printf("showing data now....");
+		printf("here is the page that made us suffer: %s",
+			channel_node->first_attribute("xmlns:yahoo")->value());
+	}
+	cout << endl;
+	cout << "parsing succeded" << endl;
+	cin.get();
 }
