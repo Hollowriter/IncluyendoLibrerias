70e2e74a3ada34a24a01cadaeed7f7bf316209e7
819 <819>
2017-05-26 13:00:52 -0300

    arreglando cosas con XML

--- ClimaParte2/Main.cpp
+++ ClimaParte2/Main.cpp
@@ -5,6 +5,7 @@
 #include<exception>
 #include<fstream>
 #include<vector>
+#include<string>
 #include"rapidxml.hpp"
 #include"rapidxml_print.hpp"
 using namespace std;
@@ -12,12 +13,21 @@ using namespace rapidxml;
 void main() {
 	cout << "Comencing parsing document test..." << endl;
 	xml_document<> thyDoc;
+	string cosa;
 	xml_node<> * normal_node;
-	ifstream thyText("Prueba.xml");
+	ifstream thyText("C:/Users/822/Desktop/IncluyendoLibrerias/Debug/Prueba.xml");
+	// cout << thyText.is_open() << endl;
+	/*while (getline(thyText, cosa))
+	{
+		cout << cosa << '\n';
+	}
+	thyText.close();*/
 	vector<char> buffer((istreambuf_iterator<char>(thyText)), istreambuf_iterator<char>());
 	buffer.push_back('\0');
 	thyDoc.parse<0>(&buffer[0]);
 	normal_node = thyDoc.first_node("query");
+	cout << normal_node->first_attribute()->value();
+
 	for (xml_node<> * channel_node = normal_node->first_node("query"); channel_node; channel_node = channel_node->next_sibling()) 
 		// Probando con el debugger, aca me tira un error en tiempo de ejecucion
 	{
