5663d8b569db39b6d3c26b063f68f6c3f79d8e9b
Hollowriter <deadgamergx2@hotmail.com.ar>
2017-05-29 01:06:26 -0300

    problema con xml arreglado y agregado SFML

--- ClimaParte2/Main.cpp
+++ ClimaParte2/Main.cpp
@@ -8,34 +8,24 @@
 #include<string>
 #include"rapidxml.hpp"
 #include"rapidxml_print.hpp"
+#include"SFML/Network.hpp"
 using namespace std;
 using namespace rapidxml;
 void main() {
 	cout << "Comencing parsing document test..." << endl;
 	xml_document<> thyDoc;
-	string cosa;
 	xml_node<> * normal_node;
-	ifstream thyText("C:/Users/822/Desktop/IncluyendoLibrerias/Debug/Prueba.xml");
-	// cout << thyText.is_open() << endl;
-	/*while (getline(thyText, cosa))
-	{
-		cout << cosa << '\n';
-	}
-	thyText.close();*/
+	/*ifstream thyText("D:\Desktop\Repo\IncluyendoLibrerias\Prueba.xml");
+	thyText.open("Prueba.xml", ifstream::in);
+	cout << thyText.is_open() << endl;
 	vector<char> buffer((istreambuf_iterator<char>(thyText)), istreambuf_iterator<char>());
 	buffer.push_back('\0');
 	thyDoc.parse<0>(&buffer[0]);
 	normal_node = thyDoc.first_node("query");
-	cout << normal_node->first_attribute()->value();
-
-	for (xml_node<> * channel_node = normal_node->first_node("query"); channel_node; channel_node = channel_node->next_sibling()) 
-		// Probando con el debugger, aca me tira un error en tiempo de ejecucion
-	{
-		printf("showing data now....");
-		printf("here is the page that made us suffer: %s",
-			channel_node->first_attribute("xmlns:yahoo")->value());
+	printf("%s", normal_node->first_attribute("xmlns:yahoo")->value());
+	for (xml_node<> * channel_node = normal_node->first_node("results"); channel_node; channel_node = channel_node->next_sibling()){
 	}
-	cout << endl;
+	cout << endl;*/
 	cout << "parsing succeded" << endl;
 	cin.get();
 }
